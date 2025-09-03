3030597634c5c1156181b7b816847054887e3815
Yan Rudenko <yan.rudenko@gmail.com>
2025-09-03 17:27:59 +0200

    update password reset logic

--- ckanext/login_throttle/logic/action.py
+++ ckanext/login_throttle/logic/action.py
@@ -1,10 +1,11 @@
-from ckan.plugins.toolkit import get_action, chained_action, check_access, get_or_bust
 from ckanext.login_throttle.authenticator import (
     get_user_throttle,
     get_address_throttle,
     reset_user_throttle,
     reset_address_throttle,
+    get_login_throttle_key,
 )
+import ckan.plugins.toolkit as tk
 
 
 def login_throttle_user_reset(context, data_dict):
@@ -13,8 +14,8 @@ def login_throttle_user_reset(context, data_dict):
 
     address: user name
     """
-    check_access("login_throttle_user_reset", context, data_dict)
-    user = get_or_bust(data_dict, "user")
+    tk.check_access("login_throttle_user_reset", context, data_dict)
+    user = tk.get_or_bust(data_dict, "user")
     return reset_user_throttle(user)
 
 
@@ -24,8 +25,8 @@ def login_throttle_address_reset(context, data_dict):
 
     address: IP address
     """
-    check_access("login_throttle_address_reset", context, data_dict)
-    address = get_or_bust(data_dict, "address")
+    tk.check_access("login_throttle_address_reset", context, data_dict)
+    address = tk.get_or_bust(data_dict, "address")
     return reset_address_throttle(address)
 
 
@@ -35,8 +36,8 @@ def login_throttle_user_show(context, data_dict):
 
     user: user name
     """
-    check_access("login_throttle_user_show", context, data_dict)
-    user = get_or_bust(data_dict, "user")
+    tk.check_access("login_throttle_user_show", context, data_dict)
+    user = tk.get_or_bust(data_dict, "user")
     return get_user_throttle(user)
 
 
@@ -47,19 +48,28 @@ def login_throttle_address_show(context, data_dict):
     address: IP address
     """
 
-    check_access("login_throttle_address_show", context, data_dict)
-    address = get_or_bust(data_dict, "address")
+    tk.check_access("login_throttle_address_show", context, data_dict)
+    address = tk.get_or_bust(data_dict, "address")
     return get_address_throttle(address)
 
 
-@chained_action
+@tk.chained_action
 def user_update(up_func, context, data_dict):
     """
     ckanext-login-throttle: reset throttling information for updated users
     to allow new login attempts after password reset
     """
     rval = up_func(context, data_dict)
-    get_action("login_throttle_user_reset")(
+
+    tk.get_action("login_throttle_user_reset")(
         dict(context, ignore_auth=True), {"user": rval["name"]}
     )
+
+    login_throttle_key = get_login_throttle_key(tk.request, rval["name"])
+    # Need to make sure we clear the Address
+    if login_throttle_key and login_throttle_key != rval["name"]:
+        tk.get_action("login_throttle_address_reset")(
+            dict(context, ignore_auth=True), {"address": login_throttle_key}
+        )
+
     return rval
